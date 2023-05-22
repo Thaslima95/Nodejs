61891e11bac8c7bc06cbe8634ac62b7482cbfbad
Thaslima <nizuthasli15@gmail.com>
2023-05-22 18:34:43 +0530

    mongo commit

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -1,13 +1,4 @@
-/* global use, db */
-// MongoDB Playground
-// To disable this template go to Settings | MongoDB | Use Default Template For Playground.
-// Make sure you are connected to enable completions and to be able to run a playground.
-// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
-// The result of the last command run in a playground is shown on the results panel.
-// By default the first 20 documents will be returned with a cursor.
-// Use 'console.log()' to print to the debug output.
-// For more documentation on playgrounds please refer to
-// https://www.mongodb.com/docs/mongodb-vscode/playgrounds/
+
 
 // Select the database to use.
 use('EKART');
@@ -63,25 +54,192 @@ use('EKART')
 
 db.Products.deleteMany({quantity:5})
 
-db.Products.find({})
+ db.Products.find({})
 
-db.Products.updateOne({name:"Acer"},{$set:{quantity:12}})
+// db.Products.updateOne({name:"Acer"},{$set:{quantity:12}})
 
-db.Products.updateOne({name:"Tuf Gaming"},{$set:{quantity:11}})
+// db.Products.updateOne({name:"Tuf Gaming"},{$set:{quantity:11}})
 
-//upsert insert the document if not found
-db.Products.updateOne({name:"Dell Inspiron"},{
-  $set:{
-    name:"Dell Inspiron",
-    price:1006,
-    quantity:18,
-    date:Date()
-  }
-},
-{upsert:true})
+// //upsert insert the document if not found
+// db.Products.updateOne({name:"Dell Inspiron"},{
+//   $set:{
+//     name:"Dell Inspiron",
+//     price:1006,
+//     quantity:18,
+//     date:Date()
+//   }
+// },
+// {upsert:true})
+
+
+db.Products.updateMany({},{$inc:{quantity:5}})
+
+db.peaks.insertMany([
+    {
+        "name": "Everest",
+        "height": 8848,
+        "location": ["Nepal", "China"],
+        "ascents": {
+            "first": {
+                "year": 1953
+            },
+            "first_winter": {
+                "year": 1980
+            },
+            "total": 5656
+        }
+    },
+    {
+        "name": "K2",
+        "height": 8611,
+        "location": ["Pakistan", "China"],
+        "ascents": {
+            "first": {
+                "year": 1954
+            },
+            "first_winter": {
+                "year": 1921
+            },
+            "total": 306
+        }
+    },
+    {
+        "name": "Kangchenjunga",
+        "height": 8586,
+        "location": ["Nepal", "India"],
+        "ascents": {
+            "first": {
+                "year": 1955
+            },
+            "first_winter": {
+                "year": 1986
+            },
+            "total": 283
+        }
+    },
+    {
+        "name": "Lhotse",
+        "height": 8516,
+        "location": ["Nepal", "China"],
+        "ascents": {
+            "first": {
+                "year": 1956
+            },
+            "first_winter": {
+                "year": 1988
+            },
+            "total": 461
+        }
+    },
+    {
+        "name": "Makalu",
+        "height": 8485,
+        "location": ["China", "Nepal"],
+        "ascents": {
+            "first": {
+                "year": 1955
+            },
+            "first_winter": {
+                "year": 2009
+            },
+            "total": 361
+        }
+    }
+])
+
+
+db.peaks.find({name:"Makalu"})
+
+//not equal
+db.peaks.find({name:{$ne:"Makalu"}})
+
+db.peaks.find({name:{$ne:"Everest"}})
+
+//in operator
+db.peaks.find({name:{$in:['Makalu','Everest']}})
+
+db.peaks.find({location:{$in:['Pakistan','India']}})
+
+db.peaks.find({location:{$ne:['Pakistan','China']}})
+
+//implicit and operator
+db.peaks.find(
+    { "name": "Everest", "height": 8848 }
+)
+
+
+//explicit and operator
+db.peaks.find({$and:[{"name":"Lhotse"},{"height":8516}]})
 
 
 
+
+//or operator
+db.peaks.find({$or:[{"name":"Everest"},{location:{$in:['Pakistan','India']}}]})
+
+db.peaks.find(
+    { "location": { $all: ["China", "Nepal"] } }
+)
+
+//nested documents
+db.peaks.find({"ascents.total":5656})
+
+db.peaks.find({"ascents.first.year":1955})
+
+db.peaks.find({$or:[{"ascents.total":283},{"ascents.first.year":1953}]})
+
+db.peaks.find({"ascents.total":{$lte:400}})
+
+db.peaks.find().count()
+
+//limit and desc sort
+db.peaks.find().limit(2).sort({name:-1})
+
+db.peaks.find(
+    {},
+    { "_id": 0, "name": 1, "height": 1 }
+).limit(3).sort({ "height": 1 })
+
+db.peaks.find(
+    { "height": { $gt: 8700 } }
+).explain("executionStats")
+
+
+//creating index to search efficiently
+//his key accepts either 1 or -1 as a value. These values denote the index’s sorting order, with 1 indicating ascending order and -1 indicating descending
+
+db.peaks.createIndex( { "height": 1 } )
+
+db.peaks.find(
+    { "height": { $gt: 8700 } }
+).explain("executionStats")
+
+//creating index with uniquenss
+db.peaks.createIndex( { "name": 1 }, { "unique": true } )
+
+db.peaks.find(
+    { "name": "Everest" }
+).explain("executionStats")
+
+db.peaks.insertOne({
+    "name": "MakaluTest",
+    "height": 9200,
+    "location": ["India"],
+    "ascents": {
+        "first": {
+            "year": 2020
+        },
+        "first_winter": {
+            "year": 2021
+        },
+        "total": 2
+    }
+})
+
+//creating index for nested documents
+db.peaks.createIndex({"ascents.total":1})
+
+db.peaks.find({"ascents.total":{$gte:400}}).sort({"ascents.total":-1}).explain("executionStats")
 
 
 
